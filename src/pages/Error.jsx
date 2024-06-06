fd41c269d6bf52800e54b11bc7139248de159508
mithun20522 <mithun20522@iiitd.ac.in>
2024-06-07 01:55:30 +0530

    page not found UI done

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -1,6 +1,14 @@
+import { Link } from "react-router-dom"
+
 const PageNotFound = () => {
   return (
-    <div>PageNotFound</div>
+    <div className="flex flex-col gap-5 h-screen items-center justify-center">
+      <div className="border p-5">
+        <span className="text-xl text-red-500 font-bold">Error: 404</span>
+        <p className="text-md">Page not found</p>
+      </div>
+      <Link to={'/'} className="bg-slate-500 hover:bg-slate-700 px-3 py-1 text-white rounded-lg">Go back</Link>
+    </div>
   )
 }
 
